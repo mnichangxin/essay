f10655ee825165b007077293e26505a3622d7670
mnichangxin <mnichangxin@vip.qq.com>
2016-06-11 15:23:06 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 window.onload = function() {
 	
+	var btn = $('#btn');
+	console.log('1');
+	btn.addEventListener('click', bundle);
+}
+
+function bundle() {
+	console.log('2');
+	alert("Hello!");
 }
