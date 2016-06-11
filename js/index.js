4e6054de507cc9fe2a2d08ee6d7cd3dbe85550fc
mnichangxin <mnichangxin@vip.qq.com>
2016-06-11 21:01:00 +0800

    add up

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ function scrollHandler() {
 	var topHeight = document.documentElement.scrollTop || document.body.scrollTop;
     var viewHeight = document.documentElement.clientHeight || document.body.clientHeight;
 
-	if (topHeight > viewHeight) {
+	if (topHeight > viewHeight*3) {
 
 		upBtn.style.display = 'block';
 
